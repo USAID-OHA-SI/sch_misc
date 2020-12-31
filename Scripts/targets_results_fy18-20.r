5a71d8e05d1abb197a1405a33e7a56da970daaee
Miriam Hartig <mhartig@usaid.gov>
2020-12-30 20:33:31 -0500

    updated to include 3 more indicators

--- Scripts/targets_results_fy18-20.r
+++ Scripts/targets_results_fy18-20.r
@@ -3,29 +3,29 @@
 #Created by: Miriam Hartig
 #Created on: 12/22/2020
 
+library(tidyverse)
+library(ICPIutilities)
+
+# Import data and subset to indicators of interest ------------------------
 
 df_mer <- read_msd("C:/Users/mhartig/Documents/MER DATA/FY20 Q4/Pre Clean/OU_IM/MER_Structured_Datasets_OU_IM_FY18-21_20201113_v1_1.txt")
 
 glimpse(df_mer)
+df_mer%>%distinct(indicator)%>% arrange(indicator)%>% print(n=Inf)
 
-df_tx <- df_mer%>%
-filter(indicator == "TX_CURR")
+df_mer <- df_mer%>%
+filter(indicator %in% c("TX_CURR", "HTS_TST","TB_PREV", "PrEP_CURR", "VMMC_CIRC"))
 
-df_tx%>%distinct(indicator)
-df_tx%>%distinct(disaggregate)
+df_mer%>%distinct(indicator)
+df_mer%>%distinct(disaggregate)
 
-df_diff <- df_tx%>%
+
+df_diff <- df_mer%>%
   filter(disaggregate == "Total Numerator")%>%
-  group_by(countryname, fiscal_year)%>%
-  summarise(achieved = sum(cumulative, na.rm = TRUE), target = sum(targets, na.rm = TRUE))%>%
-  mutate(difference = achieved - target)
+  group_by(countryname, fiscal_year, indicator)%>%
+  summarise(cumulative = sum(cumulative, na.rm = TRUE), target = sum(targets, na.rm = TRUE))%>%
+  mutate(difference = cumulative - target)%>%
+  arrange(indicator)
 
-df_tx %>% 
-  filter(countryname == "Mozambique",
-        indicator == "TX_CURR",
-         disaggregate == "Total Numerator")%>%
-  group_by(fiscal_year)%>%
-  summarise(achieved = sum(cumulative, na.rm = TRUE), target = sum(targets, na.rm = TRUE))%>%
-  mutate(difference = achieved - target)
 
-write.csv(df_diff, "C:/Users/mhartig/Documents/Financial Data/tx_curr_target_actual_diff.csv", row.names = FALSE)
+write.csv(df_diff, "C:/Users/mhartig/Documents/RFIs/CDC underspend targets/tx_curr_target_actual_diff.csv", row.names = FALSE)
