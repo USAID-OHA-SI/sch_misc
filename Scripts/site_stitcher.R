d1a92845ce3ff9e17fe5bb9118ca85a54d60a973
Miriam Hartig <mhartig@usaid.gov>
2022-05-19 13:47:55 -0400

    Initial Comit
    code to merge site level MER data files

---
+++ Scripts/site_stitcher.R
@@ -0,0 +1,64 @@
+#PURPOSE:Combine MER site level data sets for all countries (TX_CURR and SC indicators)
+#CREATED BY: Miriam Hartig
+#CREATED ON: 16 MAY 2022
+
+# -------------------------------------------------------------------------
+
+library(tidyverse)
+library(gophr)
+library(googlesheets4)
+
+# -------------------------------------------------------------------------
+data   <- "Data"
+dataout <- "Dataout"
+images  <- "Images"
+graphs  <- "Graphics"
+
+# -------------------------------------------------------------------------
+
+#Input files:
+input <- "Data/Clean Site Level"
+
+indicators <- c("TX_CURR", "SC_ARVDISP", "SC_CURR")
+
+disaggregates <- c("DispensedARVBottles","CurrentARVBottles",
+                   "Age/Sex/HIVStatus", "Total Numerator", "Age/Sex/ARVDispense/HIVStatus")
+
+files <- dir(input, pattern = "*zip", full.names = TRUE)
+
+
+# -------------------------------------------------------------------------
+
+#Stitcher function
+
+get_data <- function(input) {
+  
+  df_mer <- gophr::read_msd(input) %>%
+    filter(indicator %in% indicators,
+           disaggregate %in% disaggregates)
+    
+  }
+
+#Run function
+##(I run the function twice because R crashes if I try to run with all the datasets at once)
+    #Just SA, TZ, Uganda, ZIM, and ZAM:
+df_combined <- purrr::map_dfr(.x = files, .f = ~get_data(.x))
+    #All remaining countries
+df_combined2 <- purrr::map_dfr(.x = files, .f = ~get_data(.x))
+df_combined3 <- purrr::map_dfr(.x = files, .f = ~get_data(.x))
+
+
+df_final <- rbind(df_combined, df_combined2, df_combined3)
+
+
+# -------------------------------------------------------------------------
+
+#Upload to googledrive
+
+sheet_write(df_final, ss = "1IreR26IOuR0JuM9JNF5bhZQ07_AfVyFu")
+
+
+#Export as .csv
+
+write.csv(df_final, "Dataout/fy22q2_sitelevel_txcurr&scindicators.csv")
+
